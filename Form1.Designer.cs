6b507565fca03b58cac1fa0de35ecd3085721828
Kirill Laskin <KirillLaskin@itcenterifmo.onmicrosoft.com>
2021-02-26 17:09:53 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿
+namespace ADO.Zachot3.Stuff
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.adventureWorks2017DataSet = new ADO.Zachot3.Stuff.AdventureWorks2017DataSet();
+            this.personBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.personTableAdapter = new ADO.Zachot3.Stuff.AdventureWorks2017DataSetTableAdapters.PersonTableAdapter();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.personBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.personTypeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameStyleDataGridViewCheckBoxColumn = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.titleDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.firstNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.middleNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lastNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.emailPromotionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.additionalContactInfoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.demographicsDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.adventureWorks2017DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personBindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(73, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Показать данные";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(270, 54);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Изменить данные";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.personTypeDataGridViewTextBoxColumn,
+            this.nameStyleDataGridViewCheckBoxColumn,
+            this.titleDataGridViewTextBoxColumn,
+            this.firstNameDataGridViewTextBoxColumn,
+            this.middleNameDataGridViewTextBoxColumn,
+            this.lastNameDataGridViewTextBoxColumn,
+            this.emailPromotionDataGridViewTextBoxColumn,
+            this.additionalContactInfoDataGridViewTextBoxColumn,
+            this.demographicsDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.personBindingSource1;
+            this.dataGridView1.Location = new System.Drawing.Point(-7, 227);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(795, 226);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // adventureWorks2017DataSet
+            // 
+            this.adventureWorks2017DataSet.DataSetName = "AdventureWorks2017DataSet";
+            this.adventureWorks2017DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // personBindingSource
+            // 
+            this.personBindingSource.AllowNew = true;
+            this.personBindingSource.DataSource = this.adventureWorks2017DataSet;
+            this.personBindingSource.Position = 0;
+            this.personBindingSource.AddingNew += new System.ComponentModel.AddingNewEventHandler(this.personBindingSource_AddingNew);
+            this.personBindingSource.DataSourceChanged += new System.EventHandler(this.personBindingSource_DataSourceChanged);
+            // 
+            // personTableAdapter
+            // 
+            this.personTableAdapter.ClearBeforeFill = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(563, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(459, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Password Enter";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(73, 129);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(136, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Сохранить изменения";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // personBindingSource1
+            // 
+            this.personBindingSource1.DataMember = "Person";
+            this.personBindingSource1.DataSource = this.personBindingSource;
+            // 
+            // personTypeDataGridViewTextBoxColumn
+            // 
+            this.personTypeDataGridViewTextBoxColumn.DataPropertyName = "PersonType";
+            this.personTypeDataGridViewTextBoxColumn.HeaderText = "PersonType";
+            this.personTypeDataGridViewTextBoxColumn.Name = "personTypeDataGridViewTextBoxColumn";
+            this.personTypeDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // nameStyleDataGridViewCheckBoxColumn
+            // 
+            this.nameStyleDataGridViewCheckBoxColumn.DataPropertyName = "NameStyle";
+            this.nameStyleDataGridViewCheckBoxColumn.HeaderText = "NameStyle";
+            this.nameStyleDataGridViewCheckBoxColumn.Name = "nameStyleDataGridViewCheckBoxColumn";
+            this.nameStyleDataGridViewCheckBoxColumn.ReadOnly = true;
+            // 
+            // titleDataGridViewTextBoxColumn
+            // 
+            this.titleDataGridViewTextBoxColumn.DataPropertyName = "Title";
+            this.titleDataGridViewTextBoxColumn.HeaderText = "Title";
+            this.titleDataGridViewTextBoxColumn.Name = "titleDataGridViewTextBoxColumn";
+            this.titleDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // firstNameDataGridViewTextBoxColumn
+            // 
+            this.firstNameDataGridViewTextBoxColumn.DataPropertyName = "FirstName";
+            this.firstNameDataGridViewTextBoxColumn.HeaderText = "FirstName";
+            this.firstNameDataGridViewTextBoxColumn.Name = "firstNameDataGridViewTextBoxColumn";
+            this.firstNameDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // middleNameDataGridViewTextBoxColumn
+            // 
+            this.middleNameDataGridViewTextBoxColumn.DataPropertyName = "MiddleName";
+            this.middleNameDataGridViewTextBoxColumn.HeaderText = "MiddleName";
+            this.middleNameDataGridViewTextBoxColumn.Name = "middleNameDataGridViewTextBoxColumn";
+            this.middleNameDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // lastNameDataGridViewTextBoxColumn
+            // 
+            this.lastNameDataGridViewTextBoxColumn.DataPropertyName = "LastName";
+            this.lastNameDataGridViewTextBoxColumn.HeaderText = "LastName";
+            this.lastNameDataGridViewTextBoxColumn.Name = "lastNameDataGridViewTextBoxColumn";
+            this.lastNameDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // emailPromotionDataGridViewTextBoxColumn
+            // 
+            this.emailPromotionDataGridViewTextBoxColumn.DataPropertyName = "EmailPromotion";
+            this.emailPromotionDataGridViewTextBoxColumn.HeaderText = "EmailPromotion";
+            this.emailPromotionDataGridViewTextBoxColumn.Name = "emailPromotionDataGridViewTextBoxColumn";
+            this.emailPromotionDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // additionalContactInfoDataGridViewTextBoxColumn
+            // 
+            this.additionalContactInfoDataGridViewTextBoxColumn.DataPropertyName = "AdditionalContactInfo";
+            this.additionalContactInfoDataGridViewTextBoxColumn.HeaderText = "AdditionalContactInfo";
+            this.additionalContactInfoDataGridViewTextBoxColumn.Name = "additionalContactInfoDataGridViewTextBoxColumn";
+            this.additionalContactInfoDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // demographicsDataGridViewTextBoxColumn
+            // 
+            this.demographicsDataGridViewTextBoxColumn.DataPropertyName = "Demographics";
+            this.demographicsDataGridViewTextBoxColumn.HeaderText = "Demographics";
+            this.demographicsDataGridViewTextBoxColumn.Name = "demographicsDataGridViewTextBoxColumn";
+            this.demographicsDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.adventureWorks2017DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personBindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        public AdventureWorks2017DataSet adventureWorks2017DataSet;
+        public System.Windows.Forms.BindingSource personBindingSource;
+        public AdventureWorks2017DataSetTableAdapters.PersonTableAdapter personTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn personTypeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn nameStyleDataGridViewCheckBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn titleDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn firstNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn middleNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lastNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn emailPromotionDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn additionalContactInfoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn demographicsDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource personBindingSource1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.SqlClient;
+
+namespace ADO.Zachot3.Stuff
+{
+    public partial class Form1 : Form
+    {
+        private AdventureWorks2017Entities CustomerContext;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+         private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "adventureWorks2017DataSet.Person". При необходимости она может быть перемещена или удалена.
+            this.personTableAdapter.Fill(this.adventureWorks2017DataSet.Person);
+            CustomerContext = new AdventureWorks2017Entities();
+            var person = CustomerContext.Person.Include(x => x.PersonType).OrderBy(x => x.LastName);
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.dataGridView1.DataSource = personBindingSource;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == Convert.ToString(1111))
+            {
+                dataGridView1.ReadOnly = false;
+                try
+                {                   
+                    MessageBox.Show("Изменения в базе доступны.");
+                    this.Refresh();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+                MessageBox.Show("Введите пароль.");
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+                
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlDataAdapter dataAdapter = new SqlDataAdapter();
+            dataAdapter.Update(adventureWorks2017DataSet.Person);
+            CustomerContext.SaveChanges();           
+
+            MessageBox.Show("Изменения в базе сохранены.");
+        }
+
+        private void personBindingSource_AddingNew(object sender, AddingNewEventArgs e)
+        {
+
+        }
+
+        private void personBindingSource_DataSourceChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
